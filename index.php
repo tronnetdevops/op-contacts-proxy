c9ebe7a6ac4ca8d24c9bc92c0f806448de9421d8
Sean Murray <smurray@Seans-MacBook-Pro.local>
2016-02-04 22:13:12 -0800

    testing...

--- index.php
+++ index.php
@@ -130,6 +130,8 @@
 					
 				} catch (Exception $e){
 					file_put_contents( dirname(__FILE__) .'/update.txt', "Issues refreshing token: " . $refresh_token);
+					
+					die()
 				}
 				
 				$ret = OPContactProxy::_create_contact($client, $_REQUEST['fname']." ".$_REQUEST['lname'], $_REQUEST['pnum'], $_REQUEST['email']);
